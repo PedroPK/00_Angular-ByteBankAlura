d06f4f8a6c2dc7af5a954223aecabba86516527d
PedroPK <pedropk@gmail.com>
2022-05-20 15:58:10 -0300

    Formatting Date and Currency
    Better SCSS to Extrato Template
    
    Changed <div> to <table> in Extrato Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule }                     from '@angular/core';
-import { FormsModule }                  from '@angular/forms';
-import { BrowserModule }                from '@angular/platform-browser';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule }   from '@angular/core';
+import { FormsModule }                                  from '@angular/forms';
+import { BrowserModule }                                from '@angular/platform-browser';
+import localePt                                         from '@angular/common/locales/pt'
 
-import { AppComponent }                 from './app.component';
+import { AppComponent }                                 from './app.component';
 
-import { NovaTransferenciaComponent }   from './nova_transferencia/nova-transferencia.component';
-import { ExtratoComponent } from './extrato/extrato.component';
+import { NovaTransferenciaComponent }                   from './nova_transferencia/nova-transferencia.component';
+import { ExtratoComponent }                             from './extrato/extrato.component';
+import { registerLocaleData }                           from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -17,7 +21,16 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
