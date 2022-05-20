9a01939012a6c59be26dc7ea350797ae74063497
PedroPK <pedropk@gmail.com>
2022-05-20 07:42:17 -0300

    Overriding the Submit event to a Custom Method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NovaTransferenciaComponent }   from './nova_transferencia/nova-transferencia.component';
 import { NgModule }                     from '@angular/core';
+import { FormsModule }                  from '@angular/forms';
 import { BrowserModule }                from '@angular/platform-browser';
 
 import { AppComponent }                 from './app.component';
+
+import { NovaTransferenciaComponent }   from './nova_transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent }                 from './app.component';
     NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
